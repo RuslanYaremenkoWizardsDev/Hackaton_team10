a16893003661af0c6662fc990049459b2823bea2
EnzeoX <enzeox@gmail.com>
2021-05-15 15:17:42 +0300

    dto and user

--- core/src/main/java/com/githib/grading/entity/User.java
+++ core/src/main/java/com/githib/grading/entity/User.java
@@ -1,4 +1,167 @@
 package com.githib.grading.entity;
 
+import java.sql.Date;
+import java.util.Objects;
+
 public class User {
+
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String login;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String nickname;
+
+    private String role;
+
+    private Date date_of_creation;
+
+    public User(long l, String firstName, String lastName, String email, String login, String nickname, String password, String phone) {
+    }
+
+
+    public User(long id,
+                String firstName,
+                String lastName,
+                String email,
+                String login,
+                String password,
+                String phoneNumber,
+                String nickname,
+                String role,
+                Date date_of_creation) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.login = login;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.nickname = nickname;
+        this.role = role;
+        this.date_of_creation = date_of_creation;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getDate_of_creation() {
+        return date_of_creation;
+    }
+
+    public void setDate_of_creation(Date date_of_creation) {
+        this.date_of_creation = date_of_creation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(nickname, user.nickname) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(phoneNumber, user.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, login, nickname, password, phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", login='" + login + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
